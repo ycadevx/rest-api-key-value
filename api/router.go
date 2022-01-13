1e7a7730ea0f3fe9c0e79962fb82e0f27d813bfb
YCA <41670350+ycak1r@users.noreply.github.com>
2022-01-13 15:51:45 +0300

    request url changed

--- api/router.go
+++ api/router.go
@@ -2,22 +2,20 @@ package api
 
 import (
 	"fmt"
-
-	"net/http"
-
 	. "github/deliveryhero/source/controller"
 	"github/deliveryhero/source/log"
 	. "github/deliveryhero/source/models"
+	"net/http"
 )
 
 func StartAPI() {
 	//endpoints
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", GetAllData)
-	mux.HandleFunc("/get", GetData)
-	mux.HandleFunc("/set", AddData)
-	mux.HandleFunc("/flush", FlushData)
+	mux.HandleFunc("/api/get-all", GetAllData)
+	mux.HandleFunc("/api/get", GetData)
+	mux.HandleFunc("/api/set", AddData)
+	mux.HandleFunc("/api/flush", FlushData)
 
 	log.Fatal.Println(http.ListenAndServe(fmt.Sprintf(":%s", API_PORT), mux))
 }
